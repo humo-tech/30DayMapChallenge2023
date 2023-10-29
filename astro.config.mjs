de602abfdb67fa0d4832df6db66625649f1808f8
humo-tech <yano.wni+git@gmail.com>
2023-10-29 16:03:05 +0000

    Apply Contents Collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
-
 import vue from '@astrojs/vue'
 import sitemap from '@astrojs/sitemap'
+
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  integrations: [vue(), sitemap()],
+  integrations: [vue(), sitemap(), mdx()],
 })
---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+// 1. `astro:content`からユーティリティをインポート
+import { z, defineCollection } from 'astro:content'
+// 2. コレクションを定義
+const days = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+  }),
+})
+// 3. コレクションを登録するために、単一の`collections`オブジェクトをエクスポート
+//    このキーは、"src/content"のコレクションのディレクトリ名と一致する必要があります。
+export const collections = {
+  days,
+}
