0c3264453c38dd14bead82881fd3726ddbdc8652
humo-tech <yano.wni+git@gmail.com>
2023-11-01 16:14:33 +0000

    install mapbox

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = '30 Day Map Challenge 2023'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
+
+export const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoieWFuby1oIiwiYSI6ImNqeGFkenQ2NzE1c3MzcW44eng1d2ppMmEifQ.K_qwYjur0Aw061tYdyo4DQ'
